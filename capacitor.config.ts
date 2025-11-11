78e4a444fdd32359356315b3a11a3e0c539f39d1
Jovanny <jovanny@gmail.com>
2025-11-10 20:19:33 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'catedral-m',
+  webDir: 'www'
+};
+
+export default config;
